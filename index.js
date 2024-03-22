1d09d2b105fe5a82b812cb6ca14fc787e248d437
mohammed mehdi <mahdimohammed237git@gmail.com>
2024-03-22 22:08:37 +0300

    Complete the logic without DOM manipulation

--- index.js
+++ index.js
@@ -1,31 +1,74 @@
-const getComputerChoice = () =>{
-    const choices = ['rock' , 'paper' , 'scissors'];
-    const choice = choices[Math.floor(3*Math.random())];
-    return choice
-}
+var computerScore = 0;
+var playerScore = 0;
 
-const playRound = (playerChoice, computerChoice) => {
-    playerChoice = playerChoice.toLowerCase();
-    console.log(computerChoice);
-    if (playerChoice === computerChoice) return 'draw';
-    switch (playerChoice){
-        case 'rock':
-            if (computerChoice === 'scissors') return 'you win!';
-            else return'computer wins';
-        break;
-        case 'scissors':
-            if (computerChoice === 'paper') return 'you win!';
-            else return 'computer wins';
-            break;
-        case 'paper':
-            if (computerChoice === 'rock') return 'you win'
-            else return 'computer wins'
-            break;
-    }
+const getComputerChoice = () => {
+    const choices = ["rock", "paper", "scissors"];
+    const choice = choices[Math.floor(3 * Math.random())];
+    return choice;
+};
+
+const getWinner = (player, computer) => {
+    winner =
+        player === computer
+            ? "tie"
+            : player === "rock" && computer === "scissors"
+            ? "player"
+            : player === "scissors" && computer === "paper"
+            ? "player"
+            : player === "paper" && computer === "rock"
+            ? "player"
+            : "computer";
+    return winner;
+};
+
+const handleClick = (value) => {
+    player = value;
+    computer = getComputerChoice();
+    console.log(`${player} vs. ${computer}`);
+    winner = getWinner(player, computer);
+    console.log(winner);
     
-} 
-
-const handleClick = (value) =>{
-    console.log(value);
-    console.log(playRound(value,getComputerChoice()));
-}
+    if (winner === "player") playerScore++;
+    if (winner === "computer") computerScore++;
+    
+    console.log(
+        `player score : ${playerScore}     computer score: ${computerScore}`
+        );
+    };
+    
+    const reset = () => {
+        playerScore = 0;
+        computerScore = 0;
+        console.log("the game has been reset");
+        console.log(
+        `player score : ${playerScore}     computer score: ${computerScore}`
+        );
+    };
+    
+    
+    // const getPlayerChoice = () => {
+    //     return prompt("please enter your choice here");
+    // };
+    
+    // const evalPlayerChoice = (choice) => {
+    //     choice = choice.toLowerCase();
+    //     while (choice !== "rock" && choice !== "paper" && choice !== "scissors") {
+    //         console.log("invalid choice");
+    //         choice = prompt("please enter a valid choice").toLocaleLowerCase();
+    //     }
+    //     return choice;
+    // };
+    
+    // const playRound = () => {
+    //     playerChoice = evalPlayerChoice(getPlayerChoice());
+    //     playerChoice = playerChoice.toLowerCase();
+    //     computerChoice = getComputerChoice();
+    //     console.log(`${playerChoice} vs. ${computerChoice}`);
+    //     winner = getWinner(playerChoice, computerChoice);
+    //     console.log(winner);
+    // };
+    
+    // for (let index = 0; index < 5; index++) {
+    //     console.log(`round ${index}`);
+    //     playRound();
+    // }
